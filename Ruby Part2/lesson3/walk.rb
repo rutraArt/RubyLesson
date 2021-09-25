61b9bf54875dff44f3f14a33b2966992d3c6df8e
rutraArt <everinartdik@gmail.com>
2021-09-25 16:07:41 +0300

    MyLesson3

---
+++ Ruby Part2/lesson3/walk.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def walk(arr, &block)
+  arr.each do |value|
+    case value
+    when Array
+      walk(value, &block)
+    when Integer
+      block.call(value)
+    end
+  end
+end
+
+arr = [[[1, 2], 3], [4, 5, 6], [7, [8, 9]]]
+
+walk(arr) { |i| puts i }
