c7c725e0a21a294d8812c54928c5a20ed3aefb78
rutraArt <everinartdik@gmail.com>
2021-10-20 20:56:02 +0300

    MyLesson7

---
+++ Ruby Part2/lesson7/integer.rb
@@ -0,0 +1,23 @@
+class Integer
+  K = 1024
+  def kilobytes
+  	self * K
+  end
+
+  def megabytes
+  	kilobytes * K
+  end
+
+  def gigabytes
+  	megabytes * K
+  end
+
+  def terabytes
+  	gigabytes * K
+  end 
+end 
+
+puts 1.kilobytes
+puts 1.megabytes
+puts 1.gigabytes
+puts 1.terabytes
