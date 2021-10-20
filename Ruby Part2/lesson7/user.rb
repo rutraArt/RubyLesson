98821493745d846a4d69884027339e71ec2d5069
rutraArt <everinartdik@gmail.com>
2021-10-20 23:10:14 +0300

    MyLesson7

--- Ruby Part2/lesson7/user.rb
+++ Ruby Part2/lesson7/user.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class User
   attr_accessor :name, :last, :middle, :email
 
   def initialize
-  	yield self
+    yield self
   end
 end
 
 user = User.new do |value|
-	value.name = 'Artur'
-	value.last = 'Chaban'
-	value.middle = 'Valerievich'
-	value.email = 'everinArtDik@gmail.com'
+  value.name = 'Artur'
+  value.last = 'Chaban'
+  value.middle = 'Valerievich'
+  value.email = 'everinArtDik@gmail.com'
 end
 
 puts user.name
