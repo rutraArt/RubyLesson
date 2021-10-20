08d02021e91b0a9753a2280143d8d8f3a0413123
rutraArt <everinartdik@gmail.com>
2021-10-20 23:09:16 +0300

    MyLesson7

---
+++ Ruby Part2/lesson7/user.rb
@@ -0,0 +1,19 @@
+class User
+  attr_accessor :name, :last, :middle, :email
+
+  def initialize
+  	yield self
+  end
+end
+
+user = User.new do |value|
+	value.name = 'Artur'
+	value.last = 'Chaban'
+	value.middle = 'Valerievich'
+	value.email = 'everinArtDik@gmail.com'
+end
+
+puts user.name
+puts user.last
+puts user.middle
+puts user.email
