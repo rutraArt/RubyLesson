14420846a0a8be1ceb88cf5b5f11d321a4fa7289
rutraArt <everinartdik@gmail.com>
2021-10-23 15:01:32 +0300

    MyLesson8

--- Ruby Part2/lesson6/hash.rb
+++ Ruby Part2/lesson6/hash.rb
@@ -4,6 +4,4 @@ arr1 = %w[red orange yellow green gray indigo violet]
 
 arr2 = %w[красный оранжевый желтый зеленый голубой синий фиолетовый]
 
-# hash = {key = arr1, valee = arr2}
-
 puts Hash[arr1.zip(arr2)]
